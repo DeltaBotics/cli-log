0c97c15e753d59a16364d42b196d424c6cc32094
Vox <116598880+Vox314@users.noreply.github.com>
2024-04-24 21:14:59 +0200

    fix 3.1

--- setup.py
+++ setup.py
@@ -13,15 +13,15 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="cli-log",
-    packages=["cli_log"],
-    version="0.1.1",
+    packages=["cli"],
+    version="0.1.2",
     license="MIT",
     description="Command line interface logging.",
     long_description=readme,
     long_description_content_type="text/markdown",
     author="DeltaBotics",
     url="https://github.com/DeltaBotics/cli-log",
-    keywords=["logging", "terminal logging", "cli logging", "terminal logger", "cli logger"],
+    keywords=["logging"],
     install_requires=["colorama"],
     classifiers=classifiers,
     project_urls={"Source": "https://github.com/DeltaBotics/cli-log"},
